178ac9fc349b2532a1c87326754ab85c69cf9c08
Saahithi <sbudharaju@gmail.com>
2018-12-21 18:34:45 -0500

    Update FoodDiary.R

--- FoodDiary.R
+++ FoodDiary.R
@@ -1,9 +1,16 @@
-library(haven)
-getwd()
 setwd("~/Desktop/Research/Food_Diary")
 
-food.diary.uva.data <- read_sav("TGFoodDiaryUVA.sav")
-food.diary.mturk.w.data <- read_sav("ThanksgivingFoodDiarymTurkWed.sav")
-food.diary.mturk.t.data <- read_sav("ThanksgivingFoodDiarymTurkThurs.sav")
-food.diary.mturk.f.data <- read_sav("ThanksgivingFoodDiarymTurkFri.sav")
+# import data
+food.diary.uva.data <- read.csv("TGGoodDiaryUVA.csv")
+food.diary.mturk.w.data <- read.csv("TGFoodDiarymTurkW.csv")
+food.diary.mturk.t.data <- read.csv("TGFoodDiarymTurkT.csv")
+food.diary.mturk.f.data <- read.csv("TGFoodDiarymTurkFri.csv")
 
+# save rows with no worker ID/test
+noID <- food.diary.mturk.w.data[c(215, 216, 217, 218),]
+w.test <- food.diary.mturk.w.data[3:5,]
+t.test <- food.diary.mturk.t.data[3:5,]
+
+# remove saved rows from data frame (they'll be added back after merging)
+food.diary.mturk.w.data <- food.diary.mturk.w.data[-c(3, 4, 5, 215, 216, 217, 218),]
+food.diary.mturk.t.data <- food.diary.mturk.t.data[-c(3, 4, 5),]
