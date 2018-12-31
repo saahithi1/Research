ab76e3b4d01d87385542a1be99a87bf08f8f931c
Saahithi <sbudharaju@gmail.com>
2018-12-31 14:32:14 +0530

    Update FoodDiary.R

--- FoodDiary.R
+++ FoodDiary.R
@@ -3,36 +3,56 @@ library(dplyr)
 library(purrr)
 library(tidyverse)
 
-# import data
-food.diary.uva.data <- read.csv("TGGoodDiaryUVA.csv", stringsAsFactors = FALSE)
-food.diary.mturk.w.data <- read.csv("TGFoodDiarymTurkW.csv", stringsAsFactors = FALSE)
-food.diary.mturk.t.data <- read.csv("TGFoodDiarymTurkT.csv", stringsAsFactors = FALSE)
-food.diary.mturk.f.data <- read.csv("TGFoodDiarymTurkFri.csv", stringsAsFactors = FALSE)
+# import data (removing first 2 rows for easy merging)
+w.mturk.data <- read.csv("TGFoodDiarymTurkW.csv", header = TRUE, stringsAsFactors = FALSE)[-c(1,2),]
+t.mturk.data <- read.csv("TGFoodDiarymTurkT.csv", header = TRUE, stringsAsFactors = FALSE)[-c(1,2),]
+f.mturk.data <- read.csv("TGFoodDiarymTurkFri.csv", header = TRUE, stringsAsFactors = FALSE)[-c(1,2),]
+uva.data <- read.csv("TGGoodDiaryUVA.csv", header = TRUE, stringsAsFactors = FALSE)[-c(1,2),]
 
-# merge mturk datasets
-# from https://stackoverflow.com/questions/8091303/simultaneously-merge-multiple-data-frames-in-a-list
-all.mturk <- list(food.diary.mturk.w.data, food.diary.mturk.t.data, food.diary.mturk.f.data) %>% reduce(full_join, by = "Q47")
 
-# new variable "day" for the number of days completed
-all.mturk$day <- NA
-for (i in 3:214){                               # for each row in Wednesday dataset
-  value <- food.diary.mturk.w.data$Q47[i]       # store id as value
-  count <- 1                                    # initial count is 1 because they participated on Wednesday    
-  if (value %in% food.diary.mturk.t.data$Q47)   # check if ID is in Thursday dataset
-  {count <- count + 1}                          # if so, add 1 to the count
-  if (value %in% food.diary.mturk.f.data$Q47)   # check if ID is in Friday dataset
-  {count <- count + 1}                          # if so, add 1 to the count
-  
-  all.mturk$day[i] <- count                     # store final count for row
+###########################################################################
+########################## merging all responses ##########################
+###########################################################################
+
+# new variable for which dataset participant came from
+uva.data$original_dataset <- "TGGoodDiaryUVA"
+w.mturk.data$original_dataset <- "TGFoodDiarymTurkW"
+t.mturk.data$original_dataset <- "TGFoodDiarymTurkT"
+f.mturk.data$original_dataset <- "TGFoodDiarymTurkFri"
+
+# merge mturk data
+mturk.merge <- bind_rows(list(w.mturk.data, t.mturk.data, f.mturk.data))
+
+# new variable to differentiate between UVA vs MTurk participant
+uva.data$participant_type <- "UVA"
+mturk.merge$participant_type <- "MTurk"
+
+# calculate number of days completed for mturk participants * 
+mturk.table <- table(mturk.merge$Q47)
+mturk.merge$number_of_days <- NA
+for (i in 1:368){
+  value <- mturk.merge$Q47[i]
+  count <- as.numeric(mturk.table[names(mturk.table)==value])
+  mturk.merge$number_of_days[i] <- count
 }
 
-# new variable for mTurk or UVA participant
-all.mturk$participant <- "MTurk"
-food.diary.uva.data$participant <- "UVA"
+# calculate number of days completed for UVA participants
+uva.data$UVA_ID <- tolower(uva.data$UVA_ID)   # change values to all lowercase
+uva.data$UVA_ID[49] <- "sm4ztg"               # one person added @virginia.edu so I manually edited it
 
-# merge uva and mturk data
-all.data <- bind_rows(all.mturk, food.diary.uva.data)
+uva.table <- table(uva.data$UVA_ID)
+uva.data$number_of_days <- NA
+for (i in 1:122){
+  value <- uva.data$UVA_ID[i]
+  count <- as.numeric(uva.table[names(uva.table)==value])
+  uva.data$number_of_days[i] <- count
+}
+
+# merge mturk and uva data
+merge.1 <- bind_rows(list(mturk.merge, uva.data))
+
+# changing NA to blanks
+merge.1[is.na(merge.1)] <- ""
 
 # export .csv
-# << you can change the name of the variable by changing "merged">>
-write.csv(all.data, file="merged.csv")
+write.csv(merge.1, file="merge1.csv")
